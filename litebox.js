0038327045730f85369509ded5f412badf893d1d
Luke Evers <me@lukevers.com>
2012-12-28 13:47:28 -0500

    Figured out how to show the lightbox.
    Apparently having nothing as the link wasn't working. But now since I added
    just simply "#" as the href link, it's working now.

--- litebox.js
+++ litebox.js
@@ -2,6 +2,6 @@ function litebox() {
 	var items = document.getElementsByClassName("litebox");
 	for(var i = 0; i < items.length; i++) {
 		items[i].style.display = "block";
-		items[i].style.opacity = 1;
-		}
+		items[i].style.opacity="1";
+	}
 }
