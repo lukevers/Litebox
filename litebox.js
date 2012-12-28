898fc7caf5a8e992d4e1f1d2f9d5136b796aa0ec
Luke Evers <me@lukevers.com>
2012-12-28 08:45:34 -0500

    Initial commit

---
+++ litebox.js
@@ -0,0 +1,39 @@
+window.onload = resize();
+
+
+/*
+$(document).ready(function(){
+	$(window).resize(function(){
+		$('.center, .signup, .problem').css({
+		   position:'absolute',
+		   left: ($(window).width() 
+			 - $('.center, .signup, .problem').outerWidth())/2,
+		   top: ($(window).height() - 100
+			 - $('.center, .signup, .problem').outerHeight())/2
+		});	
+ 	});
+	$(window).resize();
+	$(window).resize();
+});
+
+function signup() {
+	$('.lightbox').fadeIn();
+	$('.signup').fadeIn();
+}
+
+function problem() {
+	$('.lightbox').fadeIn();
+	$('.problem').fadeIn();
+}
+
+//Close lightbox by escape key
+$(document).ready(function(){
+    $(document).bind('keydown', function(e) { 
+        if (e.which == 27) {
+           $('.lightbox').fadeOut();
+		   $('.signup').fadeOut();
+		   $('.problem').fadeOut();
+		}
+    });
+});
+*/
