afb5d17dd643d702f48f69cf2187e6750147cccf
Luke Evers <me@lukevers.com>
2012-12-28 14:42:27 -0500

    Instead of using javascript, CSS is the main culprit now.
    Also added three different lightbox links for examples

--- litebox.js
+++ litebox.js
@@ -1,7 +1,3 @@
 function litebox() {
-	var items = document.getElementsByClassName("litebox");
-	for(var i = 0; i < items.length; i++) {
-		items[i].style.display = "block";
-		items[i].style.opacity="1";
-	}
+	//TODO: escaping the lightbox.
 }
